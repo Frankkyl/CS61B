bf4b89b628b7ec4e533320b205ffa566ae876a79
kyfstll <liulin_sh@hotmail.com>
2017-03-19 16:14:54 -0700

    Add files via upload

---
+++ hw1/SimpleIO.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kysft on 3/19/17.
+ */
+
+import java.io.*;
+
+
+public class SimpleIO {
+
+        public static void main(String[] args) throws Exception{
+
+            BufferedReader keybd =new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Output is: " + keybd.readLine());
+        }
+}
