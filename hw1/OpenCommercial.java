dee96693af0c1fd80a935b286aad4658a2a6448b
kyfstll <liulin_sh@hotmail.com>
2017-03-22 17:08:25 -0700

    update solution

--- hw1/OpenCommercial.java
+++ hw1/OpenCommercial.java
@@ -1,7 +1,11 @@
+/**
+ * Created by kysft on 3/22/17.
+ */
 /* OpenCommercial.java */
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 /**  A class that provides a main function to read five lines of a commercial
  *   Web page and print them in reverse order, given the name of a company.
@@ -9,25 +13,62 @@ import java.io.*;
 
 class OpenCommercial {
 
-  /** Prompts the user for the name X of a company (a single string), opens
-   *  the Web site corresponding to www.X.com, and prints the first five lines
-   *  of the Web page in reverse order.
-   *  @param arg is not used.
-   *  @exception Exception thrown if there are any problems parsing the 
-   *             user's input or opening the connection.
-   */
-  public static void main(String[] arg) throws Exception {
+    /** Prompts the user for the name X of a company (a single string), opens
+     *  the Web site corresponding to www.X.com, and prints the first five lines
+     *  of the Web page in reverse order.
+     *  @param arg is not used.
+     *  @exception Exception thrown if there are any problems parsing the
+     *             user's input or opening the connection.
+     */
+    public static void main(String[] arg) throws Exception {
 
-    BufferedReader keyboard;
-    String inputLine;
+        BufferedReader keyboard;
+        String inputLine;
 
-    keyboard = new BufferedReader(new InputStreamReader(System.in));
+        keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-    System.out.print("Please enter the name of a company (without spaces): ");
-    System.out.flush();        /* Make sure the line is printed immediately. */
-    inputLine = keyboard.readLine();
+        System.out.print("Please enter the name of a company (without spaces): ");
+        System.out.flush();        /* Make sure the line is printed immediately. */
+        inputLine = keyboard.readLine();
 
     /* Replace this comment with your solution.  */
 
-  }
+        String u="http://www."+inputLine+".com";//"http://" is must, only "www" would not help
+        URL url=new URL(u);
+        //read the web address
+        InputStream isu=url.openStream();
+        InputStreamReader isr= new InputStreamReader(isu);
+        BufferedReader bf=new BufferedReader(isr);
+
+        String currentline="";
+        String[] str= new String[5];
+        ArrayList al = new ArrayList();
+        HashMap hm=new HashMap();
+        System.out.println();
+        System.out.println("<----normal order--->");
+        for(int i=0;i<5;i++){
+           currentline= bf.readLine();//each time u run bf.readLine(), it read the line, so you need save the data first
+           System.out.println(currentline);
+           str[i]=currentline;
+           al.add(currentline);
+           hm.put(i,currentline);
+        }
+        System.out.println();
+        System.out.println("<----reverse order by HashMap--->");
+
+        for(int i=4;i>=0;i--){
+            System.out.println(hm.get(i));
+        }
+        System.out.println();
+        System.out.println("<----reverse order by Array--->");
+        for(int i=4;i>=0;i--){
+            System.out.println(str[i]);
+        }
+        System.out.println();
+        System.out.println("<----reverse order by ArrayList--->");
+        for(int i=4;i>=0;i--){
+            System.out.println(al.get(i));
+        }
+
+    }
 }
