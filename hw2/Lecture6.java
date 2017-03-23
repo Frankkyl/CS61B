4f88721811f8ab5708b1b5956cfe110f0ec4e3fb
kyfstll <liulin_sh@hotmail.com>
2017-03-22 23:38:12 -0700

    Add files via upload

---
+++ hw2/Lecture6.java
@@ -0,0 +1,35 @@
+package hw2;
+
+/**
+ * Created by kysft on 3/22/17.
+ */
+public class Lecture6 {
+
+
+    public static void main(String[] args) {
+        //int[][] table=new int[x][y];
+        //create an array of reference to array;
+        //create an array of reference to array;
+        //Initializers;
+        //Human[] b = {kayla, rishi, new Human("Paola")};//Human is not avaliable since it's under package hw1
+        //int[][] c = {{7,3,2},{x},{8,5,0,0},{y+z,3}};
+        //compile time error:
+        //d={3,7};
+        //f({1,2,3});
+        /*int[] a,b,c; //all reference arrays
+        int d[],e,f[][];// d is 1d,e not array,f is 2d
+        int[] h, i[];//h is 1d, i is 2d*/
+        /*Fraction[] f=new Fraction[5];
+        for(int =0;i<5;i++){
+            f[i]=new Frantion(i,6);
+        }*/
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);// show the things u type by command line
+        }
+    }
+
+    class Echo {
+
+    }
+}
